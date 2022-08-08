0c22bcd20f47e8c8280ad09c45eab4e53ee8a746
Chris Molnar <christopherstevenmolnar@gmail.com>
2022-08-08 14:18:19 -0700

    update 1

--- js/ProductsController.js
+++ js/ProductsController.js
@@ -1,14 +1,18 @@
 class ProductsController {
-    constructor (name, description, author, price, image, items, id){
+    constructor (name, description, author, price, image, items, currentId = 0){
         this.name = name;
         this.description = description;
         this.author = author;
         this.price = price;
         this.image = image;
         this.items = [];
-        this.id = 0++;
+        this.currentId =;
     }
 
+
+    // ProductsController.addItem(name, description, image)
+
+    
     function addItems(){
         return this.items.push();
         }
