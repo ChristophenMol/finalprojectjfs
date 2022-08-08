8df2b6c6e53f0a4d373cc58bd9b9a5cff42b46b4
Chris Molnar <christopherstevenmolnar@gmail.com>
2022-08-08 13:58:27 -0700

    new function

--- js/ProductsController.js
+++ js/ProductsController.js
@@ -1,13 +1,19 @@
 class ProductsController {
-    constructor (name, description, author, price, image, items){
+    constructor (name, description, author, price, image, items, id){
         this.name = name;
         this.description = description;
         this.author = author;
         this.price = price;
         this.image = image;
         this.items = [];
+        this.id = 0++;
     }
-}
-const i = new ProductsController();
 
-console.log(i.items);
+    function addItems(){
+        return this.items.push();
+        }
+}
+
+// const i = new ProductsController();
+
+
