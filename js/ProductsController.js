f66749d554300ad468212f452413aed5c73070e4
Chris Molnar <christopherstevenmolnar@gmail.com>
2022-08-08 14:54:31 -0700

    fixed confusion

--- js/ProductsController.js
+++ js/ProductsController.js
@@ -1,23 +1,28 @@
 class ProductsController {
-    constructor (name, description, author, price, image, items, currentId = 0){
-        this.name = name;
-        this.description = description;
-        this.author = author;
-        this.price = price;
-        this.image = image;
+    constructor (currentId = 0){
         this.items = [];
-        this.currentId =;
+        this.currentId = currentId;
     }
 
+     addItems(name, author, description, image, price, createdAt){
+        const newProduct = {
+            id: this.currentId++,
+            name: name,
+            author: author,
+            description: description,
+            image: image,
+            price: price,
+            createdAt: createdAt
 
-    // ProductsController.addItem(name, description, image)
-
-    
-    function addItems(){
-        return this.items.push();
         }
-}
 
-// const i = new ProductsController();
+        this.items.push(newProduct);
+        }
+    }
+
+ const i = new ProductsController();
+ console.log(i.items);
+ i.addItems("flamethrow", "Banksy", "graffiti", "image.jpg", 5000000, "Berlin 2011");
+ console.log(i.items[0]);
 
 
